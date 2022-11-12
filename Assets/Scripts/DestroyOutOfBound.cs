d52e4578ab4c64764d7aea825b3cabc2baa02ac2
catsnakedog <0812kdh@gmail.com>
2022-11-12 17:35:01 +0900

    11.12
    합치기

---
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBound : MonoBehaviour
+{
+    private float leftLimit = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < -leftLimit)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
