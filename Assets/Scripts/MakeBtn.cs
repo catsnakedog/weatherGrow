df68a07e0a199a8605c5286e02f82f343dfef8e7
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2022-11-16 19:29:10 +0900

    Ingame Setup
    인게임 일시정지 후 재개 카운트다운 구현

---
+++ Assets/Scripts/MakeBtn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBtn : MonoBehaviour
+{
+    private GameObject btn1;
+    private GameObject btn2;
+    private GameObject btn3;
+    private GameObject btn4;
+
+
+}
