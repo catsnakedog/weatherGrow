4804146cf9f9f0f42a1d03d5195ac3a549dfeeae
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2022-11-23 19:43:38 +0900

    Script Fixed
    변수 추가, 버튼 스크립트 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,20 @@ public class GameManager : MonoBehaviour
 
     }
 
+
+    #region 설정 변수
     public static bool GameIsPaused = false; //게임 일시정지 변수
+    #endregion
+
+    #region 인게임 변수
+    public List<GameObject> weatherList = new List<GameObject> { };
+    public float speed;
+    public int season; // 계절 봄/여름/가을/겨울 0/1/2/3
+    public int[] nowState = new int[3]; //현재 입력된 정답
+    public int hp;
+
+
+    #endregion
 
 }
+
--- Assets/Scripts/MakeBtn.cs
+++ Assets/Scripts/MakeBtn.cs
@@ -1,13 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MakeBtn : MonoBehaviour
 {
-    private GameObject btn1;
-    private GameObject btn2;
-    private GameObject btn3;
-    private GameObject btn4;
+    //시계 방향으로 버튼 할당
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+
+    //각 버튼에 해당하는 날씨 1234 1234 1245 1634 해/비/구름/바람/안개/눈
+    private int[] btnFunc1 = new int[4];
+
+    //계절에 따른 버튼 할당 매서드
+    public void Update()
+    {
+        if (GameManager.instance.season == 0)
+        {
+            btnFunc1[0] = 1;
+            btnFunc1[1] = 2;
+            btnFunc1[2] = 3;
+            btnFunc1[3] = 4;
+            btn1.onClick.AddListener(CheckAns1);
+
+        }
+        else if (GameManager.instance.season == 1)
+        {
+            btnFunc1[0] = 1;
+            btnFunc1[1] = 2;
+            btnFunc1[2] = 3;
+            btnFunc1[3] = 4;
+        }
+        else if (GameManager.instance.season == 2)
+        {
+            btnFunc1[0] = 1;
+            btnFunc1[1] = 2;
+            btnFunc1[2] = 4;
+            btnFunc1[3] = 5;
+        }
+        else if (GameManager.instance.season == 3)
+        {
+            btnFunc1[0] = 1;
+            btnFunc1[1] = 6;
+            btnFunc1[2] = 3;
+            btnFunc1[3] = 4;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void CheckAns1()
+    {
+
+    }
+    private int CheckAns2()
+    {
+        return btnFunc1[1];
+    }
+    private int CheckAns3()
+    {
+        return btnFunc1[2];
+    }
+    private int CheckAns4()
+    {
+        return btnFunc1[3];
+    }
 
 
 }
