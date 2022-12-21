bbcd99ce1ac3b58a37de295f06d39ba2fb283d22
Winteryun <118039226+Winteryun@users.noreply.github.com>
2022-12-21 14:53:02 +0900

    현재점수, 최고점수

--- Assets/Scripts/Dongyun/Scripts/UI/TextManager.cs
+++ Assets/Scripts/Dongyun/Scripts/UI/TextManager.cs
@@ -9,22 +9,29 @@ public class TextManager : MonoBehaviour
   public Text BestScore ;
   public Text CurrentScore ;
  
-int _BestScore = 0 ;
-int _CurrentScore = 0 ; 
+    //public int bestScore;
+    //public int crruentScore;
 
     void Start()
     {
+      
+      if(GameManager.instance.bestScore <=GameManager.instance.crruentScore) 
+      {
+        GameManager.instance.bestScore = GameManager.instance.crruentScore ; 
+        PlayerPrefs.SetInt("BestScore",GameManager.instance.bestScore) ;
+      }
 
-       BestScore.text = $"{_BestScore}" ; 
-        CurrentScore.text = $"{_CurrentScore}" ; 
-     
+      
+      int LoadScore = PlayerPrefs.GetInt("BestScore") ; 
+       BestScore.text = (LoadScore / 48).ToString() + "년 " + ((LoadScore%48) / 4).ToString() + "월 " + (LoadScore%4).ToString() + "주 "; 
+       CurrentScore.text = (GameManager.instance.crruentScore / 48).ToString() + "년 " + ((GameManager.instance.crruentScore%48) / 4).ToString() + "월 " + (GameManager.instance.crruentScore%4).ToString() + "주 ";
     }
   
 
     // Update is called once per frame
     void Update()
     {
-      
+       
     }
 
 }
