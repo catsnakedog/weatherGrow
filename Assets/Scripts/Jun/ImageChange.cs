0ddad7777954730bd440b831783c0eb1d8127342
dosawas <dosawas@naver.com>
2023-02-22 16:34:06 +0900

    buttonimage
    change button img

---
+++ Assets/Scripts/Jun/ImageChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChange : MonoBehaviour
+{
+    public Sprite[] chageImg;
+    public Image myimage;
+
+    void Start()
+    {
+        myimage = GetComponent<Image>();
+    }
+
+    void Update()
+    { 
+        myimage.sprite = chageImg[GameManager.instance.season];
+    }
+}
