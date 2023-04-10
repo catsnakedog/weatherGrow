11c7b40a7caec501eb27e7fa590fd5d6c2aebf4b
catsnakedog <0812kdh@gmail.com>
2023-04-11 01:23:42 +0900

    뉴스 대사 완료

--- Assets/Scripts/Dongyun/Scripts/UI/NewsSpawn.cs
+++ Assets/Scripts/Dongyun/Scripts/UI/NewsSpawn.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class NewsSpawn : MonoBehaviour
 {
-    public Text _text;
-    public new List<string> newsContent = new List<string> {"(종합) 대기 매우 건조, 화재 유의, 낮과 밤의 기온차 큼" ,
+    int random;
+    public Text text;
+    public GameObject news;
+    public List<string> newsContent = new List<string> {
+        "(종합) 대기 매우 건조, 화재 유의, 낮과 밤의 기온차 큼" ,
         "(오늘) 전국 대체로 맑음" ,
         "(내일) 전국 가끔 구름많음" ,
         "(모레) 중부지방 가끔 구름많음, 남부지방과 제주도 대체로 흐림, 제주도 가끔 비" ,
@@ -24,5 +28,25 @@ public class NewsSpawn : MonoBehaviour
         "(오늘~내일) 대체로 맑다가 내일 밤부터 구름많아짐" ,
         "(모레) 대체로 흐림" };
 
+    void Start()
+    {
+        random = Random.Range(0, newsContent.Count);
+        text.text = newsContent[random];
+    }
+    void Update()
+    {
+        news.transform.Translate(Vector3.left * GameManager.instance.speed * Time.deltaTime);
+        Debug.Log(news.transform.position.y);
+        if (news.transform.position.x < -7.7f)
+        {
+            NewsSetting();
+        }
+    }
 
+    void NewsSetting()
+    {
+        news.transform.position = new Vector3(7.7f, 1.125f);
+        random = Random.Range(0, newsContent.Count);
+        text.text = newsContent[random];
+    }
 }
